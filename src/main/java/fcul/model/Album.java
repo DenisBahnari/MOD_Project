c07712315c83622d88ef5b6b2c4f76c6341ef1a5
HarvyDev <franciscolbantunes@gmail.com>
2026-04-08 19:16:45 +0100

    Created Album and User entities

---
+++ src/main/java/fcul/model/Album.java
@@ -0,0 +1,69 @@
+package fcul.model;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    // --- Ownership ---
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    // --- Photos in this album ---
+    @OneToMany(mappedBy = "album", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Photo> photos = new ArrayList<>();
+
+    // --- Constructors ---
+    protected Album() {} // required by Hibernate
+
+    public Album(String name, User owner) {
+        this.name = name;
+        this.owner = owner;
+    }
+
+    // --- Helper methods ---
+    public void addPhoto(Photo photo) {
+        photos.add(photo);
+        photo.getMetadata().setAlbum(this);
+    }
+
+    public void removePhoto(Photo photo) {
+        photos.remove(photo);
+        photo.getMetadata().setAlbum(null);
+    }
+
+    // --- Getters & Setters ---
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+}
---
+++ src/main/java/fcul/model/User.java
@@ -0,0 +1,54 @@
+package fcul.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String username;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Album> albums = new ArrayList<>();
+
+    // --- Constructors ---
+    protected User() {}
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    // --- Helper methods ---
+    public void addAlbum(Album album) {
+        albums.add(album);
+        album.setOwner(this);
+    }
+
+    public void removeAlbum(Album album) {
+        albums.remove(album);
+        album.setOwner(null);
+    }
+
+    // --- Getters & Setters ---
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
